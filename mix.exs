f311317ea312e387120115cc47f0f249471c28a3
matsa <alexandre.lprtr@gmail.com>
2023-05-15 15:02:47 -0400

    Bump versions

--- mix.exs
+++ mix.exs
@@ -4,12 +4,12 @@ defmodule Squid.MixProject do
   def project do
     [
       app: :squid,
-      version: "0.1.0",
+      version: "0.1.1",
       build_path: "./_build",
       config_path: "./config/config.exs",
       deps_path: "./deps",
       lockfile: "./mix.lock",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
